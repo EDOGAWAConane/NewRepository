b6decc1d06338fd8a92442ade68aef9738cf6b59
EDOGAWAConane <boulmane.oumayma@outlook.fr>
2020-03-10 22:41:25 +0100

    developper commit

---
+++ NewRepository-master/src/MyJavaClass.java
@@ -0,0 +1,10 @@
+
+public class MyJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Master : here is the first code !");
+		System.out.println(" Developper : Adding !");
+	}
+
+}
